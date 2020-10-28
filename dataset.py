24feb28f244653d322248d9ba645ad26d8638570
Patrick Düggelin <patrick.dueggelin@live.de>
2020-10-28 09:29:21 +0100

    Initial update
    Include snr_db flag
    Include oversample_noise flag
    Format code with black

--- dataset.py
+++ dataset.py
@@ -4,52 +4,60 @@ import random
 from collections import namedtuple
 
 
-AudioVisualEntry = namedtuple('AudioVisualEntry', ['speaker_id', 'audio_path', 'video_path'])
+AudioVisualEntry = namedtuple(
+    "AudioVisualEntry", ["speaker_id", "audio_path", "video_path"]
+)
 
 
 class AudioVisualDataset:
+    def __init__(self, base_path):
+        self._base_path = base_path
 
-	def __init__(self, base_path):
-		self._base_path = base_path
+    def subset(self, speaker_ids, max_files=None, shuffle=False):
+        entries = []
 
-	def subset(self, speaker_ids, max_files=None, shuffle=False):
-		entries = []
+        for speaker_id in speaker_ids:
+            audio_paths = glob.glob(
+                os.path.join(self._base_path, speaker_id, "audio", "*.wav")
+            )
 
-		for speaker_id in speaker_ids:
-			audio_paths = glob.glob(os.path.join(self._base_path, speaker_id, 'audio', '*.wav'))
+            for audio_path in audio_paths:
+                entry = AudioVisualEntry(
+                    speaker_id,
+                    audio_path,
+                    AudioVisualDataset.__audio_to_video_path(audio_path),
+                )
+                entries.append(entry)
 
-			for audio_path in audio_paths:
-				entry = AudioVisualEntry(speaker_id, audio_path, AudioVisualDataset.__audio_to_video_path(audio_path))
-				entries.append(entry)
+        if shuffle:
+            random.shuffle(entries)
 
-		if shuffle:
-			random.shuffle(entries)
+        return entries[:max_files]
 
-		return entries[:max_files]
+    def list_speakers(self):
+        return os.listdir(self._base_path)
 
-	def list_speakers(self):
-		return os.listdir(self._base_path)
+    @staticmethod
+    def __audio_to_video_path(audio_path):
+        path_names = audio_path.split("/")
+        if path_names[-2] != "audio":
+            raise Exception("invalid audio-video path conversion")
 
-	@staticmethod
-	def __audio_to_video_path(audio_path):
-		path_names = audio_path.split('/')
-		if path_names[-2] != 'audio':
-			raise Exception('invalid audio-video path conversion')
+        path_names[-2] = "video"
 
-		path_names[-2] = 'video'
-
-		return glob.glob(os.path.splitext('/'.join(path_names))[0] + ".*")[0]
+        return glob.glob(os.path.splitext("/".join(path_names))[0] + ".*")[0]
 
 
 class AudioDataset:
+    def __init__(self, base_paths):
+        self._base_paths = base_paths
 
-	def __init__(self, base_paths):
-		self._base_paths = base_paths
+    def subset(self, max_files=None, shuffle=False):
+        audio_file_paths = [
+            os.path.join(d, f) for d in self._base_paths for f in os.listdir(d)
+        ]
 
-	def subset(self, max_files=None, shuffle=False):
-		audio_file_paths = [os.path.join(d, f) for d in self._base_paths for f in os.listdir(d)]
+        if shuffle:
+            random.shuffle(audio_file_paths)
 
-		if shuffle:
-			random.shuffle(audio_file_paths)
-
-		return audio_file_paths[:max_files]
+        return audio_file_paths[:max_files]
